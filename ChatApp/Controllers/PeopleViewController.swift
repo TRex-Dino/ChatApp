a97eef1e2d05cafaf53b655ffff1fce82cb572f5
Dima <dmitrysinkov6@gmail.com>
2021-07-10 23:41:14 +0300

    add ProfileViewController

--- ChatApp/Controllers/PeopleViewController.swift
+++ ChatApp/Controllers/PeopleViewController.swift
@@ -26,6 +26,7 @@ class PeopleViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSearchBar()
         setupCollectionView()
         createDataSource()
         reloadData(with: nil)
